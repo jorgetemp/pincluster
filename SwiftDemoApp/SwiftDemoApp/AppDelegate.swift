15e87faee7e98f961d535a0db9e5fe621d13e88a
Jorge Siqueira <jorge.siqueira@gmail.com>
2017-04-03 18:35:29 +0200

    Swift version Update

--- SwiftDemoApp/SwiftDemoApp/AppDelegate.swift
+++ SwiftDemoApp/SwiftDemoApp/AppDelegate.swift
@@ -25,8 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
 
-  func application(application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     if kMapsAPIKey.isEmpty {
       fatalError("Please provide an API Key using kMapsAPIKey")
     }
